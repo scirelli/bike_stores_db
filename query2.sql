090741b10ec22a9c6c160221952e1b8e6d01602d
scirelli <scirelli+git@gmail.com>
2023-11-20 12:53:44 -0500

    Adding another query

--- query2.sql
+++ query2.sql
@@ -12,3 +12,22 @@ GROUP BY production.products.product_id
 ORDER BY "TotalSold" DESC
 LIMIT 10
 ;
+
+-- Another way to write it.
+SELECT
+	pp.product_name,
+	q."TotalSold"
+FROM
+	(SELECT
+		pp.product_id,
+		SUM(sales.order_items.quantity) AS "TotalSold"
+	FROM sales.orders
+	  INNER JOIN sales.order_items ON sales.orders.order_id = sales.order_items.order_id
+	  INNER JOIN production.products AS pp ON sales.order_items.product_id = pp.product_id
+	GROUP BY pp.product_id
+
+	) AS q
+    INNER JOIN production.products AS pp ON q.product_id = pp.product_id
+ORDER BY "TotalSold" DESC
+LIMIT 10
+;
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build -t biker-postgres --no-cache=true .
+#docker build -t biker-postgres --no-cache=true .
 
 # docker run \
 #     --name bikedb \
@@ -9,3 +9,10 @@ docker build -t biker-postgres --no-cache=true .
 #     -p 5432:5432 \
 #     --network dbnetwork \
 #     biker-postgres
+
+docker run -p 8080:80 \
+    --name pgAdmin \
+    --network dbnetwork \
+    --env 'PGADMIN_DEFAULT_EMAIL=example@cirelli.org' \
+    --env 'PGADMIN_DEFAULT_PASSWORD=password' \
+    --detach dpage/pgadmin4
